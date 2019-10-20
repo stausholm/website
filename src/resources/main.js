a22217aec6d00190f4568cdfef7e871c494b2fef
Morten <muffix26@gmail.com>
2019-10-20 16:14:27 +0200

    add parcel module bundler

---
+++ src/resources/main.js
@@ -0,0 +1,21 @@
+import "./main.scss";
+
+// babel/preset-env is used to convert ES6 syntax to ES5. 
+// If new ES6 library functions, such as string.includes is needed, install the individual polyfill or use @babel/polyfill (if using babel < v7.4) https://babeljs.io/docs/en/next/babel-polyfill.html
+
+console.log('nice')
+
+const x = "123";
+
+const hasVal = x.includes('4')
+console.log(hasVal)
+const test = `this is ${x} test`
+
+const arrow = () => {
+  return 'this is arrow func'
+}
+
+const obj1 = {x} // this is a comment
+
+const obj2 = {...obj1}
+
