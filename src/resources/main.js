40174863346afb40619bdae0498cd18da3107bb1
Morten <muffix26@gmail.com>
2019-10-20 16:37:58 +0200

    try and fail make parcel polyfill es6 features

--- src/resources/main.js
+++ src/resources/main.js
@@ -4,6 +4,8 @@ import "./main.scss";
 // If new ES6 library functions, such as string.includes is needed, install the individual polyfill or use @babel/polyfill (if using babel < v7.4) https://babeljs.io/docs/en/next/babel-polyfill.html
 
 console.log('nice')
+const set = new Set([1, 2, 3]);
+[1, 2, 3].includes(2);
 
 const x = "123";
 
