85b943ddf67dc290efd63e072eb8707006d80a51
Morten <muffix26@gmail.com>
2019-11-03 22:00:12 +0100

    alt list styles

--- src/resources/content/posts-alt.js
+++ src/resources/content/posts-alt.js
@@ -2,16 +2,19 @@ export default [
   {
     title: 'wow',
     link: 'http://google.com/save',
-    description: 'Lorem ipsum ctetur adipi.'
+    description: 'Lorem ipsum ctetur adipi.',
+    isExternal: true
   },
   {
     title: 'wow',
     link: 'http://google.com/',
-    description: 'Lorem ipsum dolor sit amet, consectetuquat.'
+    description: 'Lorem ipsum dolor sit amet, consectetuquat.',
+    isExternal: true
   },
   {
     title: 'wow',
     link: 'http://google.com',
-    description: 'Lorem ipsum dolor sit ametconsequat.'
+    description: 'Lorem ipsum dolor sit ametconsequat.',
+    isExternal: true
   }
 ]
--- src/resources/js/post-alt.js
+++ src/resources/js/post-alt.js
@@ -3,24 +3,28 @@ import posts from '../content/posts-alt'
 
 const createPostMarkup = post => {
   return `
-  <li class="squiggly">
+  <li>
     <a href="${post.link}" title="Explore ${post.title}">
-      <span>${post.title}</span>
-      <span class="subtext">${post.description}</span>
-      <div class="arrow-icon" aria-hidden="true">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="presentation">
-          <g fill="currentColor">
-            <g>
-              <path d="M0 0h24v24H0z" fill="none"></path>
-              <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
-            </g>
+      <div class="linkwrapper">
+        <span class="headtext">${post.title}</span>
+        <br>
+        <span class="subtext">${post.description}</span>
+      </div>
+      <div class="arrow-icon ${post.isExternal ? 'rotate' : ''}" aria-hidden="true">
+        <svg class="active" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="presentation">
+          <g>
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
+          </g>
+        </svg>
+        <svg class="inactive" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="presentation">
+          <g>
+            <path d="M0 0h24v24H0z" fill="none"></path>
+            <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
           </g>
         </svg>
       </div>
     </a>
-    <svg aria-hidden="true" role="presentation" class="squiggly-line" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 390 50">
-      <path fill="none" vector-effect="non-scaling-stroke" d="M0,47.585c0,0,97.5,0,130,0c13.75,0,28.74-38.778,46.168-19.416C192.669,46.5,243.603,47.585,260,47.585c31.821,0,130,0,130,0"/>
-    </svg>
   </li>`
 }
 
