e3bbe7b6a054ca1f68af284f26979951f3d11eaa
Morten <muffix26@gmail.com>
2019-11-23 18:37:56 +0100

    update footer

--- src/resources/content/links.js
+++ src/resources/content/links.js
@@ -8,17 +8,17 @@ export default [
   },
   {
     icon: icons.codepen,
-    url: 'https://github.com/stausholm',
-    title: 'explore Codepen'
+    url: 'https://codepen.io/Stausholm',
+    title: 'explore CodePen'
   },
   {
     icon: icons.facebook,
-    url: 'https://github.com/stausholm',
+    url: 'https://www.facebook.com/morten.s.simonsen.7',
     title: 'stalk Facebook'
   },
   {
     icon: icons.linkedin,
-    url: 'https://github.com/stausholm',
+    url: 'https://www.linkedin.com/in/morten-stausholm-simonsen-75a300140/',
     title: 'stalk LinkedIn'
   }
 ]
--- src/resources/js/footer.js
+++ src/resources/js/footer.js
@@ -4,7 +4,7 @@ import links from '../content/links'
 const createFooterMarkup = link => {
   return `
   <div class="col col-25 col-s-100">
-    <a href="${link.url}" class="footer-link" title="${link.title}">
+    <a href="${link.url}" target="_blank" rel="noopener noreferrer" class="footer-link" title="${link.title}">
       <span class="shadow"></span>
       <span>${link.title}</span>
       <div class="icon">
